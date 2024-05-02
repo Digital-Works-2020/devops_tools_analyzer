6fdb0110c218852d3e8a43a5b5fddc0462e37a9b
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-02 20:25:45 +0530

    TAD-2 Import Views

--- devops_tools_analyzer/urls.py
+++ devops_tools_analyzer/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, re_path
+from . import views
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
