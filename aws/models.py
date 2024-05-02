f88ffaa3ca624b888fd44d6cd21b105bc10a7226
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-02 19:57:33 +0530

    TAD-3 Finalized AWS Model

--- aws/models.py
+++ aws/models.py
@@ -4,3 +4,4 @@ class AWSAccount(models.Model):
     account_name = models.CharField(max_length=60,primary_key=True)
     access_key = models.TextField(unique=True)
     secret_key = models.TextField(unique=True)
+    aws_session_token = models.TextField()
