a58c4dc164f3baff9d3e003163ab3113699c440e
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-02 19:58:32 +0530

    TAD-2 AWS init Cost Analyzer

--- aws/urls.py
+++ aws/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, re_path
 from . import views
-
+from .api.cost_analysis import AWSCostAnalyzer
 urlpatterns = [
+        re_path(r'\?aws_account=(?P<aws_account>\w+)/',views.index , name='aws_accounts'),
+        re_path('cost/',AWSCostAnalyzer.as_view()),
         re_path(r'^$', views.index, name='index'),
 ]
