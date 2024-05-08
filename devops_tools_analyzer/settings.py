62ba85ddb8a2d3c32e0c961ee67e248d930eb533
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-08 16:41:28 +0530

    TAD-5 Adding Global Context to Retain menu items

--- devops_tools_analyzer/settings.py
+++ devops_tools_analyzer/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'devops_tools_analyzer.global_context.get_global_context',
             ],
         },
     },
