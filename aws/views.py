633de66eda7868c84f3aee71ac7af629214be57f
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-08 16:39:11 +0530

    TAD-5 Adding AWS View

--- aws/views.py
+++ aws/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import urllib
+import re
 
-def index(reques,aws_account):
-    return HttpResponse("<h1>The AWS Homepage</h1>")
+def aws_index(request,**kwargs):
+    context = {}
+    decoded_url = urllib.parse.unquote(request.build_absolute_uri())
+    match = re.search(r'aws_account=(\w+)', decoded_url)
+    if match:
+        context["aws_account"] = match.group(1)
+    return render(request, 'aws_page.html',context)
