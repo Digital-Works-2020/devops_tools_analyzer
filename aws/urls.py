75fd4b3e7883770e9f850b9f7ab331075cbeec12
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-09 19:18:23 +0530

    TAD-6 IAM URL's

--- aws/urls.py
+++ aws/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, re_path
 from . import views
 from .api.cost_analysis import AWSCostAnalyzer
+from .api.iam_analysis import AWSIAMAnalyzer
+
 urlpatterns = [
         re_path(r'\?aws_account=(?P<aws_account>\w+)',views.aws_index , name='aws_accounts'),
         re_path('/cost$',AWSCostAnalyzer.as_view()),
+        re_path('/iam$',AWSIAMAnalyzer.as_view()),
 ]
