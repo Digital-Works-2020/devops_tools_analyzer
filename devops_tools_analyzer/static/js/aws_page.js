bb0b28a4e8498379415276090d617911ee7a32d3
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-14 19:56:09 +0530

    TAD-8 AWS JS for Route53

--- devops_tools_analyzer/static/js/aws_page.js
+++ devops_tools_analyzer/static/js/aws_page.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
    current_cost_api_url = "/aws/cost?account_name=" + aws_account_name + "&data_to_extract=total_cost"
    prev_cost_api_url = current_cost_api_url + "&period=prev"
    groups_url = "/aws/iam?account_name=CS_Dev&data_to_extract=groups"
+   route53_url = "/aws/route53?account_name=" + aws_account_name  + "&data_to_extract=route53"
    
    $.get(current_cost_api_url,function(data,status){
        if(data["status"] == 200)
@@ -43,5 +44,19 @@ $(document).ready(function() {
           $('.empty_groups')[0].inner_HTML = data["error"]
        }
    });   
+   $.get(route53_url,function(data,status){
+       if(data["status"] == 200)
+       {
+          console.log("Updating Hosted Zones & Domains Details")
+          $('.total_hosted_zones')[0].innerHTML = data["total_hosted_zones"]
+          $('.total_domains')[0].innerHTML = data["total_domains"]
+       }
+       else
+       {
+          console.log("Updating Error Message")
+          $('.total_hosted_zones')[0].inner_HTML = data["error"]
+          $('.total_domains')[0].inner_HTML = data["error"]
+       }
+   });    
 }
 )
