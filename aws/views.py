e758cb8a1ca610eac017206d522c0f0ac8d7de62
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-14 19:54:10 +0530

    TAD-8 Add Views for AWS Hosted Zones

--- aws/views.py
+++ aws/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 import urllib
 import re
 
+def aws_hosted_zones(request,**kwargs):
+    context = {}
+    decoded_url = urllib.parse.unquote(request.build_absolute_uri())
+    match = re.search(r'aws_account=(\w+)', decoded_url)
+    if match:
+        context["aws_account"] = match.group(1)
+    return render(request,'aws_route53_hosted_zones.html',context) 
+
 def aws_index(request,**kwargs):
     context = {}
     decoded_url = urllib.parse.unquote(request.build_absolute_uri())
