f98646c487bf4c8421a7d73f9ef0cff872528a0f
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-05-28 20:28:35 +0530

    TAD-8 CSS For Hosted zones page

---
+++ devops_tools_analyzer/static/css/aws_hosted_zones.cs
@@ -0,0 +1,27 @@
+/* CSS For Data tables */
+.dataTables_wrapper .dataTables_filter input
+{
+    background-color:white !important;
+}
+.dataTables_wrapper .dataTables_length select
+{
+    background-color:white !important;
+}
+table
+{
+    background-color:white !important;
+}
+
+/* Loader CSS */
+.loader-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255, 255, 255, 0.8);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
