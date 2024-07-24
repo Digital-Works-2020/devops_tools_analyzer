964f602dcf47e4e657481ba2d7ebf4042b775f58
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-07-24 19:30:09 +0530

    TAD-11 Revamp URL's

--- aws/urls.py
+++ aws/urls.py
@@ -5,9 +5,10 @@ from .api.iam_analysis import AWSIAMAnalyzer
 from .api.route53_analysis import AWSRoute53Analyzer
 
 urlpatterns = [
-        re_path('\?aws_account=(?P<aws_account>\w+)/hosted_zones',views.aws_hosted_zones,name='aws_hosted_zones'),
-        re_path(r'\?aws_account=(?P<aws_account>\w+)',views.aws_index , name='aws_accounts'),
-        re_path('/cost$',AWSCostAnalyzer.as_view()),
-        re_path('/iam$',AWSIAMAnalyzer.as_view()),
-        re_path('/route53$',AWSRoute53Analyzer.as_view()),
+        re_path(r'(?P<aws_account>\w+)/hostedzones/(?P<zone_id>\w+)/', views.aws_zone_details, name='aws_zone'),
+        re_path(r'(?P<aws_account>\w+)/hostedzones', views.aws_hosted_zones,name='aws_hosted_zones'),
+        re_path('cost$',AWSCostAnalyzer.as_view()),
+        re_path('iam$',AWSIAMAnalyzer.as_view()),
+        re_path('route53$',AWSRoute53Analyzer.as_view()),
+        re_path(r'(?P<aws_account>\w+)$',views.aws_index , name='aws_accounts'),
 ]
