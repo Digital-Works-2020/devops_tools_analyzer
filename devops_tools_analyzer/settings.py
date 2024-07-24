681432fe97d911e9a85b7ab2ea25d73612d0a0b3
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-07-24 19:35:23 +0530

    TAD-11 Mimetypes & Whitenoise MiddleWare

--- devops_tools_analyzer/settings.py
+++ devops_tools_analyzer/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'devops_tools_analyzer.urls'
