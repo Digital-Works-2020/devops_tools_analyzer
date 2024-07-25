d468adb3ba2f4a8c191b7038c90c52e9f6599c18
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-07-25 19:17:14 +0530

    TAD-11 Url Extra Slash Removal

--- aws/urls.py
+++ aws/urls.py
@@ -5,7 +5,7 @@ from .api.iam_analysis import AWSIAMAnalyzer
 from .api.route53_analysis import AWSRoute53Analyzer
 
 urlpatterns = [
-        re_path(r'(?P<aws_account>\w+)/hostedzones/(?P<zone_id>\w+)/', views.aws_zone_details, name='aws_zone'),
+        re_path(r'(?P<aws_account>\w+)/hostedzones/(?P<zone_id>\w+)', views.aws_zone_details, name='aws_zone'),
         re_path(r'(?P<aws_account>\w+)/hostedzones', views.aws_hosted_zones,name='aws_hosted_zones'),
         re_path('cost$',AWSCostAnalyzer.as_view()),
         re_path('iam$',AWSIAMAnalyzer.as_view()),
