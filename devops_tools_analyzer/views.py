6c81ac2319ac865e56f3f13672249599ca07a27e
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-04 17:26:18 +0530

    TAD-7 Home Pages View - Jira

--- devops_tools_analyzer/views.py
+++ devops_tools_analyzer/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from aws.models import AWSAccount
+from atlassian_jira.models import JIRAAccount
 
 def index(request):
     aws_objects = AWSAccount.objects.all()
+    jira_objects = JIRAAccount.objects.all()    
     context = {
-                 'aws_objects' : aws_objects
+                 'aws_objects' : aws_objects,
+                 'jira_objects' : jira_objects
     }
     return render(request, "base.html",context)
