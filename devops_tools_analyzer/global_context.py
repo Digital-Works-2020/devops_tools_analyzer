eca71767e3f8b38ed781d32779744a29d73dc36b
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-04 17:18:57 +0530

    TAD-7 Jira in Global Context

--- devops_tools_analyzer/global_context.py
+++ devops_tools_analyzer/global_context.py
@@ -1,7 +1,10 @@
 from aws.models import AWSAccount
+from atlassian_jira.models import JIRAAccount
 
 def get_global_context(request):
     aws_objects = AWSAccount.objects.all()
+    jira_objects = JIRAAccount.objects.all()
     return {
-        'aws_objects' : aws_objects
+        'aws_objects' : aws_objects,
+        'jira_objects' : jira_objects
     }
