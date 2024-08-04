40335127aee1089d837db2d2b9cbf67f89971d00
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-04 17:34:04 +0530

    TAD-7 Jira Views

---
+++ atlassian_jira/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import urllib
+import re
+
+def jira_index(request,jira_account):
+    context = {}
+    context["jira_account"] = jira_account
+    return render(request, 'jira_page.html',context)
