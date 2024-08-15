5d829af20ec5703cbfa2857c5bdf4f30a99a82ec
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-15 15:09:08 +0530

    TAD-9 Setup Project & Index Page Jira URL's

--- atlassian_jira/urls.py
+++ atlassian_jira/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, re_path
 from . import views
+from .api.project_analysis import JIRAProjectAnalyzer
 
 urlpatterns = [
-        re_path(r'(?P<jira_account>\w+)$',views.jira_index , name='jira_accounts'),
+        re_path('project$',JIRAProjectAnalyzer.as_view()),
+        re_path(r'(?P<jira_account>\w+)$',views.jira_index , name='jira_accounts')
 ]
