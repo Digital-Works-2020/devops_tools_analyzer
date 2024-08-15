ffde3d49216634b6cfc1eebc59a88e8df72951df
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-15 15:05:22 +0530

    TAD-9 Add Base URL Field

--- atlassian_jira/models.py
+++ atlassian_jira/models.py
@@ -4,3 +4,4 @@ class JIRAAccount(models.Model):
     account_name = models.CharField(max_length=60,primary_key=True)
     user_email = models.TextField()
     user_token = models.TextField()
+    base_url = models.URLField(max_length=200)
