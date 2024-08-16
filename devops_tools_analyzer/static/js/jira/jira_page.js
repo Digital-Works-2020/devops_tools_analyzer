3984fe2b54239a26dc5bf9ae9d546af6a98e5770
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-16 13:24:16 +0530

    TAD-9 Jira JS - Webhooks Code

--- devops_tools_analyzer/static/js/jira/jira_page.js
+++ devops_tools_analyzer/static/js/jira/jira_page.js
@@ -2,7 +2,23 @@ $(document).ready(function() {
    jira_account_name =  $('.account_name')[0].innerHTML.split(":")[1].trim();
    
    project_api_url = "/jira/project?account_name=" + jira_account_name + "&data_to_extract=project_types"
-   
+   webhook_api_url = "/jira/webhook?account_name=" + jira_account_name + "&data_to_extract=webhook_details"
+
+   $.get(webhook_api_url,function(data,status){
+       if(data["status"] == 200)
+       {
+          console.log("Updating Jira Webhooks")
+          $('.total_webhooks')[0].innerHTML =  data["data"]["total_webhooks"]
+          $('.disabled_webhooks')[0].innerHTML =  data["data"]["disabled_webhooks"]
+       }
+       else
+       {
+          console.log("Updating Error Message for Webhooks")
+          $('.total_webhooks')[0].innerHTML =  "Failed to Process Data"
+          $('.disabled_webhooks')[0].innerHTML =  "Failed to Process Data"
+       }
+   });
+
    $.get(project_api_url,function(data,status){
        if(data["status"] == 200)
        {
@@ -15,12 +31,11 @@ $(document).ready(function() {
        }
        else
        {
-          console.log("Updating Error Message")
+          console.log("Updating Error Message for Jira Projects")
           $('.business_projects')[0].innerHTML =  "Failed to Process Data"
           $('.software_projects')[0].innerHTML =  "Failed to Process Data"
           $('.team_projects')[0].innerHTML =  "Failed to Process Data"
           $('.company_projects')[0].innerHTML =  "Failed to Process Data"
-          console.log(data["error"])
        }       
    });
 }
