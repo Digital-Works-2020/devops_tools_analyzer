da2d0bca644d90756adc4e7b7ed403e44179a418
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-16 13:22:13 +0530

    TAD-9 Add WebHook URL Path

--- atlassian_jira/urls.py
+++ atlassian_jira/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, re_path
 from . import views
 from .api.project_analysis import JIRAProjectAnalyzer
+from .api.webhook_analysis import JIRAWebhookAnalyzer
 
 urlpatterns = [
         re_path('project$',JIRAProjectAnalyzer.as_view()),
+        re_path('webhook$',JIRAWebhookAnalyzer.as_view()),
         re_path(r'(?P<jira_account>\w+)$',views.jira_index , name='jira_accounts')
 ]
