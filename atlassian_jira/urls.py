39ca78697e113cefc9ac897e7200e4a34e2edce2
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-08-18 17:11:45 +0530

    TAD-21 Add Plugin Analyzer URL

--- atlassian_jira/urls.py
+++ atlassian_jira/urls.py
@@ -2,9 +2,11 @@ from django.urls import include, re_path
 from . import views
 from .api.project_analysis import JIRAProjectAnalyzer
 from .api.webhook_analysis import JIRAWebhookAnalyzer
+from .api.plugin_analysis import JIRAPluginAnalyzer
 
 urlpatterns = [
         re_path('project$',JIRAProjectAnalyzer.as_view()),
         re_path('webhook$',JIRAWebhookAnalyzer.as_view()),
+        re_path('plugin$',JIRAPluginAnalyzer.as_view()),
         re_path(r'(?P<jira_account>\w+)$',views.jira_index , name='jira_accounts')
 ]
