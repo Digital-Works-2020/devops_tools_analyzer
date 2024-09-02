3282a8b3c4ea579ec3e376ed12d47810ccfe337e
Paduchuri Manideep <paduchurimanideep@gmail.com>
2024-09-02 18:39:20 +0530

    TAD-21 Plugin Details URL mapping

--- atlassian_jira/urls.py
+++ atlassian_jira/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
         re_path('project$',JIRAProjectAnalyzer.as_view()),
         re_path('webhook$',JIRAWebhookAnalyzer.as_view()),
         re_path('plugin$',JIRAPluginAnalyzer.as_view()),
+        re_path(r'(?P<jira_account>\w+)/plugin_details', views.plugin_details,name='jira_plugin_details'),
         re_path(r'(?P<jira_account>\w+)$',views.jira_index , name='jira_accounts')
 ]
